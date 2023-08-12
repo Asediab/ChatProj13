ed9987a388702cffa161f4cf8128a041217d3c77
Bohdan <asediaboli@gmail.com>
2023-08-12 20:06:45 +0200

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
+import firebase from "firebase/compat";
 
+
+const config = {
+  apiKey: 'fw2VsJiFRw52M0WXOVtWk_N8VPydGvIy2JNBVCKaYDY',
+  databaseURL: 'https://proj13-9e457-default-rtdb.europe-west1.firebasedatabase.app/'
+};
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +13,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'chat-proj12';
+  constructor() {
+    firebase.initializeApp(config);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RoomlistComponent } from './roomlist/roomlist.component';
+import { AddroomComponent } from './addroom/addroom.component';
+import { ChatroomComponent } from './chatroom/chatroom.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RoomlistComponent,
+    AddroomComponent,
+    ChatroomComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
